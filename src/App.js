7b47a40c05bd5938c2f62633440c87089982714f
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-11-24 18:09:52 +0530

    sdk integrated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { WorldIDWidget } from "@worldcoin/id";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WorldIDWidget
+        actionId="wid_BPZsRJANxct2cZxVRyh80SFG" // obtain this from developer.worldcoin.org
+        signal="my_signal"
+        enableTelemetry
+        onSuccess={(verificationResponse) => console.log(verificationResponse)} // pass the proof to the API or your smart contract
+        onError={(error) => console.error(error)}
+        debug={true} // to aid with debugging, remove in production
+      />
     </div>
   );
 }
